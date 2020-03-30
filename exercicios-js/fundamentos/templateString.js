1b242398bf0ae58cd2e8121af82d072befc9aa30
José Willyan <willyansilvagomes2020@gmail.com>
2020-03-30 17:47:37 -0300

    aprendendo javascript

--- exercicios-js/fundamentos/templateString.js
+++ exercicios-js/fundamentos/templateString.js
@@ -1,5 +1,6 @@
 const nome = 'Rebeca'
 const concatenação = 'Olá ' + nome + '!'
+let kaka = 2+5
 const template = `
     Olá
     ${nome}!`
@@ -12,4 +13,4 @@ console.log('1 + 1 =', 1+1)
 const up = texto => texto.toUpperCase()
 console.log(`Ei... ${up('cuidado')}!`) //Provavemmente meu cérebro vai abster isso pq eu ja aprendi de outra forma kkkkkkkkkkkkkkkkk
 console.log('Ei... '+up('cuidado')+'!')
-console.log(2+5)
+console.log(`Consegui ${kaka*2}`)
