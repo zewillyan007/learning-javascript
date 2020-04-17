66a72cd74a1f6a4f2ae9d87a439833394754ee52
José Willyan <willyansilvagomes2020@gmail.com>
2020-04-17 17:56:37 -0300

    java

--- exercicios-js/fundamentos/certificado.js
+++ exercicios-js/fundamentos/certificado.js
@@ -93,6 +93,7 @@ const HTMLCertificado = nome => `
     participou da <a target="_blank" href="https://www.alura.com.br/quarentenadev">quarentenaDev </a>
     <span>
       de 30/03/2020 a 10/04/2020
+      Carga horária de 10h
     </span>
   </p>
 
